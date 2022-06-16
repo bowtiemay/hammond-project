ca31285c52ec726c8741f81efd0d8157003574a1
bowtiemay <62029215+bowtiemay@users.noreply.github.com>
2022-06-15 21:57:40 -0700

    Create Point.js

---
+++ Point.js
@@ -0,0 +1,17 @@
+class Point{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    
+  }
+
+  point.push({x:x, y:y});
+  show(){
+    //have point show up on screen
+    
+  }
+  
+}
+
+var p1 = new Point(3, 4);
+var p2 = new Point(5, 7);
